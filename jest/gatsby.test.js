271cdd571927fe750e287efd5c7d7c80ea1afebc
sajjadhossain <sajjad@withpulp.com>
2018-08-19 19:36:30 -0400

    added jest.timeout

--- jest/gatsby.test.js
+++ jest/gatsby.test.js
@@ -1,11 +1,13 @@
 const server = require('./jest-puppeteer.config').server;
 import puppeteer from 'puppeteer'
 
+jest.setTimeout(30000);
+
 describe('Gatsby installation', () => {
   it('should have a #___gatsby element', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
+
     await page.goto(server.baseUrl)
     await page.waitForSelector('#___gatsby')
 
