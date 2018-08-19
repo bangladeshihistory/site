89aa3523cdbefe01208a6c538993576b444ea4e6
sajjadhossain <sajjad@withpulp.com>
2018-08-19 19:51:47 -0400

    set timers to fake, added async

--- jest/gatsby.test.js
+++ jest/gatsby.test.js
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer'
 jest.setTimeout(30000);
 
 describe('Gatsby installation', () => {
-  it('should have a #___gatsby element', () => {
+  it('should have a #___gatsby element', async () => {
     const browser = puppeteer.launch();
     const page = browser.newPage();
 
