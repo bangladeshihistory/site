40a0b8b2e835f653a89d0d3233aef8099699fe51
sajjadhossain <sajjad@withpulp.com>
2018-08-19 18:58:33 -0400

    added jest and circleci

---
+++ jest/gatsby.test.js
@@ -0,0 +1,29 @@
+const server = require('./jest-puppeteer.config').server;
+import puppeteer from 'puppeteer'
+
+let browser
+let page
+
+beforeAll(async () => {
+  browser = await puppeteer.launch(
+    {
+      headless: false,
+      slowMo: 100,
+    }
+  )
+  page = await browser.newPage()
+})
+
+afterAll(async () => {
+  await browser.close();
+})
+
+describe('Gatsby installation', () => {
+  beforeAll(async () => {
+    await page.goto(server.baseUrl)
+  })
+
+  it('should have a #___gatsby element', async () => {
+    await page.waitForSelector('#___gatsby')
+  })
+})
---
+++ jest/jest-puppeteer.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  server: {
+    baseUrl: 'http://192.168.56.95/',
+    command: './node_modules/.bin/gatsby serve',
+    port: 9000
+  },
+}
