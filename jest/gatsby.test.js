0e9dfd4da0cf78d5fcd01e9c3181fe01b56cc6d8
sajjadhossain <sajjad@withpulp.com>
2018-08-19 20:20:02 -0400

    headed browser set

--- jest/gatsby.test.js
+++ jest/gatsby.test.js
@@ -5,8 +5,8 @@ jest.setTimeout(30000);
 
 describe('Gatsby installation', () => {
   it('should have a #___gatsby element', async () => {
-    const browser = puppeteer.launch()
-    const page = browser.pages()
+    const browser = puppeteer.launch({ headless: false })
+    const page = browser.newPage()
 
     await page.goto(server.baseUrl)
     await page.waitForSelector('#___gatsby')
