618b66e23475fa498ea7421fef25daec8414f812
sajjadhossain <sajjad@withpulp.com>
2018-08-19 20:29:17 -0400

    fixed tests locally

--- jest/gatsby.test.js
+++ jest/gatsby.test.js
@@ -1,16 +1,14 @@
 const server = require('./jest-puppeteer.config').server;
-import puppeteer from 'puppeteer'
-
-jest.setTimeout(30000);
 
 describe('Gatsby installation', () => {
-  it('should have a #___gatsby element', async () => {
-    const browser = puppeteer.launch({ headless: false })
-    const page = browser.newPage()
+  const puppeteer = require('puppeteer');
 
-    await page.goto(server.baseUrl)
+  it('should have a #___gatsby element', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(server.baseUrl);
     await page.waitForSelector('#___gatsby')
 
     await browser.close();
-  })
+  });
 })
